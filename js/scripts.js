4a0bf73808522f4f3b7e23d85316686db92c65b5
PatriciaDuranM <patduran16@gmail.com>
2024-11-19 14:57:19 +0100

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,27 @@
 const acordionElement = document.getElementById("acordion");
 const acordionTextElement = document.getElementById("acordion-text");
+const menuElement = document.getElementById("menu");
+const burgerElement = document.getElementById("burger");
 
+/*menu*/
+
+const toggleMenu = () => {
+  menuElement.classList.toggle("menu-show");
+  document.body.classListtoggle("showing-menu");
+
+  if (menuElement.classList.contains("menu-show")) {
+    burgerElement.src = "./assets/icons/close.svg";
+  } else {
+    burgerElement.src = "assets/icons/hamburger.svg";
+  }
+};
+
+burgerElement.addEventListener("click", toggleMenu);
+
+/*acordeon*/
 const showText = (event) => {
   console.log(event.target);
-  event.target.children[0].classList.toggle["acordion-show"];
+  event.target.children[1].classList.toggle("acordion-show");
 };
 
 acordionElement.addEventListener("click", showText);
